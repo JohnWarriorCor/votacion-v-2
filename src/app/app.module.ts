7f683f53d3df61eaebbe5316c7d8d13c618d0a4f
John David <guerrerocordobajohn@gmail.com>
2023-01-31 21:05:08 -0500

    aplicativo terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING,
+    BrowserAnimationsModule,
+    NgbModule,
+    RouterModule,
+    FormsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
